6b0ffcb5dc79e55a84c88075d8aacd5e898c4726
Peter de Verteuil <p.verteuil@gmail.com>
2017-04-26 17:20:17 -0400

    added logic to include 500px-api-keys from secret file as 'ENV' variables that can be accessed anywhere on the platform

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,10 @@ module Challenge500px
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    file_path = File.join(Rails.root, 'config', 'five_hundred_secret.yml')
+    YAML.load(File.open(file_path)).each do |key, value|
+      ENV[key.to_s] = value
+    end if File.exists?(file_path)
   end
 end
