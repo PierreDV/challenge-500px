b1728d64fdacbfa658f3ccc44603c2f0452b4d73
Peter de Verteuil <p.verteuil@gmail.com>
2017-04-26 17:48:39 -0400

    added code to 'config/application.rb' to load secret api keys onto platform and initializer to configure '500px' gem with a valid 'consumer_key' and 'consumer_secret'

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,13 @@ module Challenge500px
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    
+
+    # This loads the api keys stored in 'config/five_hundred_secret.yml' so they can be accessed
+    # through out the platform.
     file_path = File.join(Rails.root, 'config', 'five_hundred_secret.yml')
     YAML.load(File.open(file_path)).each do |key, value|
       ENV[key.to_s] = value
     end if File.exists?(file_path)
+
   end
 end
---
+++ config/initializers/five_hundred_px.rb
@@ -0,0 +1,4 @@
+F00px.configure do |config|
+  config.consumer_key = ENV['consumer_key']
+  config.consumer_secret = ENV['consumer_secret']
+end
