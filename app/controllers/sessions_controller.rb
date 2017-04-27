7122fbfcfd0513261d84446feff06d4e90c13eb2
Peter de Verteuil <p.verteuil@gmail.com>
2017-04-27 16:09:01 -0400

    Refactored Session Controller 'create' action so 500px token is stored as a session variable, added methods in Session Helper to log in user, and a 'log_out' method which destroys user_token and sets @current_user to nil

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    log_in params[:oauth_token]
+    omniauth = request.env["omniauth.auth"]
+    session[:user_token] = omniauth["credentials"]["token"] if omniauth['provider'] == '500px'
     redirect_to root_url # Not the final implementation!
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,15 +1,17 @@
 module SessionsHelper
-  # Sets oauth token as session variable.
-  def log_in(user_token)
-    session[:user_token] = user_token
-  end
-
-  # Sets @current_user variable if one is not already set.
+  # Sets @current_user to session's :user_token, and sets to nil if session value doesn't exist.
   def current_user
     @current_user ||= session[:user_token]
   end
+
   # Returns true the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
+  end
+
+  # Delete's 500px token saved in session and logs out user.
+  def log_out
+    session.delete(:user_token)
+    @current_user = nil
   end
 end
