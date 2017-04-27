ccb7b4c618aebb5b2b4d9d35edd6f6d7277ddd35
Peter de Verteuil <p.verteuil@gmail.com>
2017-04-27 19:49:11 -0400

    refactored code to be more legible and added comments to controller actions explaining their purpose

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,16 @@
 class SessionsController < ApplicationController
-  def new
-  end
-
+  # This action is called by 500px's OmniAuth callback URL. It stores user credentials as a session variable.
   def create
     omniauth = request.env["omniauth.auth"]
-    session[:user_credentials] = {user_id: omniauth["uid"], token: omniauth["credentials"]["token"], secret: omniauth["credentials"]["secret"]}
+    session[:user_credentials] = {
+      user_id: omniauth["uid"],
+      token: omniauth["credentials"]["token"],
+      secret: omniauth["credentials"]["secret"] }
     redirect_to root_url # Not the final implementation!
   end
 
+  # Calls the Sessions Helper's 'log_out' method. Destroying the session,
+  # setting @current_user to nil and redirecting user back home.
   def destroy
     log_out
     redirect_to root_url
