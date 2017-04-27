db67911a9d6d941a0aff11342843e716ad3ad9fd
Peter de Verteuil <p.verteuil@gmail.com>
2017-04-26 21:53:36 -0400

    Used boostrap's '.thumb' and '.thumbnail' classes to create a thumbnail gallery display for images

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,6 @@
 class PhotosController < ApplicationController
   def index
     res = F00px.get('/v1/photos?feature=popular&sort=created_at&rpp=100&image_size=3')
-    decoded_res = ActiveSupport::JSON.decode(res.body)
-    @photos_hash = decoded_res["photos"]
+    @photos_hash = ActiveSupport::JSON.decode(res.body)["photos"]
   end
-
 end
