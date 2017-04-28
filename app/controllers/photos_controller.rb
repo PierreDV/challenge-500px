cd43f9152d69b91a5c7f5abfce403f2ac8489970
Peter de Verteuil <p.verteuil@gmail.com>
2017-04-27 21:34:33 -0400

    edited 'index' action to retreive 'voted' state for photos if user is logged in and removing inefficient 'photo_liked?' helper method from Sessions Helper

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,18 +2,17 @@ class PhotosController < ApplicationController
   # This code makes a JSON request to the 500px API and parses the 'photos' key and values into a hash.
   # The hash is saved as an instance variable so it will work with the view's ERB.
   def index
-    res = F00px.get('/v1/photos?feature=popular&sort=rating&rpp=100&image_size=3')
-    @photos_hash = ActiveSupport::JSON.decode(res.body)["photos"]
-
     if logged_in?
       client = F00px::Client.new
       client.token = session[:user_credentials]["token"]
       client.token_secret = session[:user_credentials]["secret"]
-
-      user_res = client.get("/v1/users/#{session[:user_credentials]["user_id"]}")
-      # @user = @photos_hash = ActiveSupport::JSON.decode(user_res.body)
-      # get_liked_photos(@user)
+      res = client.get('/v1/photos?feature=popular&sort=rating&rpp=100&image_size=3&include_states=voted')
+      @photos_hash = ActiveSupport::JSON.decode(res.body)["photos"]
+    else
+      res = F00px.get('/v1/photos?feature=popular&sort=rating&rpp=100&include_states=voted&image_size=3')
+      @photos_hash = ActiveSupport::JSON.decode(res.body)["photos"]
     end
+
   end
 
   # This code send a POST request to the 500px API liking the chosen photo.
