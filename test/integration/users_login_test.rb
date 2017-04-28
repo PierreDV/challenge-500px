a2ac2d3045ad4fb56f50b18ac9addafee609fdf6
Peter de Verteuil <p.verteuil@gmail.com>
2017-04-28 08:54:43 -0400

    Running tests will switch Omniauth to test mode, edited user_login test and added start of test for photos 'index' view

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  test "login" do
-    get login_path
+  test "Omniauth callback creates session and redirects to homepage" do
+    ENV['omniauth.auth'] = OmniAuth.config.mock_auth["500px"]
+    get '/auth/500px/callback'
+    assert_redirected_to root_path
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,16 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  OmniAuth.config.test_mode = true
+  omniauth_hash = {
+    "provider" => "500px",
+    "uid" => 1234,
+    "credentials" => {
+      "token" => "token123",
+      "secret" => "token_secret123"
+    }
+  }
 
+  OmniAuth.config.add_mock("500px", omniauth_hash)
   # Add more helper methods to be used by all tests here...
 end
