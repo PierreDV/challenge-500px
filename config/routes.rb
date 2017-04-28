35bfbda8ccefc8c1051eb9b9f30c7d1097cb946d
Peter de Verteuil <p.verteuil@gmail.com>
2017-04-28 11:16:54 -0400

    added rescue block to photos 'index' controller so that a user is redirected to a custom error page in case the 500px API fails to send a response

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root   'photos#index'
   get    'photos/like'
+  get    'request_error'  => 'photos#request_error'
   get    '/auth/500px', as: 'login'
   get    '/auth/500px/callback' => 'sessions#create'
   delete 'logout'               => 'sessions#destroy'
