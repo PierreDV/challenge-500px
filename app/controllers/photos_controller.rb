81e1293901de376c55039e438191812ea402369e
Peter de Verteuil <p.verteuil@gmail.com>
2017-04-27 20:13:11 -0400

    removed uneeded 'new' action and template from Photos Controller, started implementing a method to determine whether a user has already 'liked' a photo or not.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,12 +1,16 @@
 class PhotosController < ApplicationController
-  
   # This code makes a JSON request to the 500px API and parses the 'photos' key and values into a hash.
   # The hash is saved as an instance variable so it will work with the view's ERB.
   def index
     res = F00px.get('/v1/photos?feature=popular&sort=rating&rpp=100&image_size=3')
     @photos_hash = ActiveSupport::JSON.decode(res.body)["photos"]
 
-    # user = client.get("/v1/users/#{session[:user_credentials]["user_id"]}")
+    client = F00px::Client.new
+    client.token = session[:user_credentials]["token"]
+    client.token_secret = session[:user_credentials]["secret"]
+
+    user = client.get("/v1/users/#{session[:user_credentials]["user_id"]}")
+    get_liked_photos(user)
   end
 
   # This code send a POST request to the 500px API liking the chosen photo.
@@ -15,5 +19,16 @@ class PhotosController < ApplicationController
     client.token = session[:user_credentials]["token"]
     client.token_secret = session[:user_credentials]["secret"]
     res = client.post("/v1/photos/#{params[:photo_id]}/vote?vote=1")
+  end
+
+  def get_liked_photos(user)
+
+    votes = F00px.get("/v1/photos/#{@photos_hash[0]["id"]}/votes")
+    users = ActiveSupport::JSON.decode(votes.body)["users"]
+
+    # @photos_hash.each do | photo |
+    #   votes = F00.get("/v1/photos/#{photo["id"]}votes")
+    #   votes["people"]
+    # end
   end
 end
