d5f713bac09ad54d2b9214ab007a465c8feb8fd7
Peter de Verteuil <p.verteuil@gmail.com>
2017-04-28 11:34:21 -0400

    added Application Helper method 'full_title', this gets called in the application layout and inserts a provided :title value into a dynamically constructed title. This ensures we have unique title tags for all views, which is good SEO practice.

--- test/controllers/photos_controller_test.rb
+++ test/controllers/photos_controller_test.rb
@@ -2,7 +2,14 @@ require 'test_helper'
 
 class PhotosControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get photos_url
+    get photos_path
     assert_response :success
+    assert_select "title", "100 Most Popular | 500px Challenge"
+  end
+
+  test "should get request error" do
+    get request_error_path
+    assert_response :success
+    assert_select "title", "Request Error | 500px Challenge"
   end
 end
