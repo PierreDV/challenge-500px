1f1519a471c3c9de95c684b29324b33a9b5258d7
Peter de Verteuil <p.verteuil@gmail.com>
2017-04-28 09:15:18 -0400

    ran migration to remove warning when running tests, added more assertions to Omniauth callback test, ensuring user is redirected to root path, that the Photo's 'index' view is rendered, and that 'like' buttons are being rendered

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,8 +2,21 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
   test "Omniauth callback creates session and redirects to homepage" do
+    # We first make sure that 'like' buttons are not rendering
+    # on the homepage if they are not not logged in.
+    get root_path
+    assert_select "a[class=?]", "btn btn-info btn-xs", count: 0
+
+    # We stub Omniauth's response for valid authentication, then test
+    # to ensure succsefull login is creating the [:users_credentials] session,
+    # and redirecting the user to the root path, we ensure the Photo's 'index'
+    # view is rendered, and finally ensure the 'like' buttons are being rendered.
     ENV['omniauth.auth'] = OmniAuth.config.mock_auth["500px"]
     get '/auth/500px/callback'
+    assert session[:user_credentials]
     assert_redirected_to root_path
+    follow_redirect!
+    assert_template 'photos/index'
+    assert_select "a[class=?]", "btn btn-info btn-xs", count: 100
   end
 end
